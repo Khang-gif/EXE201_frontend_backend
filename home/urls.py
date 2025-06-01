ea25acee965efbe59b29b7394d858511e7231205
Trần Lê Duy <tranleduy@MacBookPro.lan>
2025-06-01 17:11:09 +0700

    update code base, create hompage interface

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about_us',views.aboutUs, name='about_us')
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def aboutUs(request):
+    return render(request, 'aboutUs.html')
